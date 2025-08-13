87a4e035f024b099bfcedc4523320600196820cb
catrasto16 <62905326+catrasto16@users.noreply.github.com>
2025-08-13 18:38:10 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,63 @@
+// Datos de los jugadores
+const players =[
+    { nombre: "Álex Laguna" },
+    { nombre: "Catrasto" },
+    { nombre: "Clarico" },
+    { nombre: "Juanma" },
+    { nombre: "Juanma Palomita" },
+    { nombre: "Juan Tovar" },
+    { nombre: "Juan Carlos" },
+    { nombre: "Jose Ángel" },
+    { nombre: "Juan López" },
+    { nombre: "Jose Gemelo" },
+    { nombre: "Pedro Gemelo" },
+    { nombre: "Jose Ortega" },
+    { nombre: "Puro" },
+    { nombre: "Maño" },
+    { nombre: "Mateo" },
+    { nombre: "Castilla" },
+    { nombre: "Diego Moreno" },
+    { nombre: "Juan Trepabarcos" },
+    { nombre: "Villegas" },
+    { nombre: "Pedrín" },
+    { nombre: "Santi" },
+    { nombre: "Ismael" },
+    { nombre: "Pedro" },
+    { nombre: "Floris" },
+    { nombre: "Jesús" }
+];
+
+// Datos de partidos actualizados
+const matches = [
+    {
+        jornada: 1,
+        fecha: "01/09/2025",
+        arbitro: "Pedrín",
+        equipoNegro: [
+            { nombre: "Juan Carlos", goles: 2,amarillas:1 },
+            { nombre: "Pedro Gemelo", goles: 1 },
+            { nombre: "Catrasto", goles: 4 },
+            { nombre: "Maño", goles: 3 },
+            { nombre: "Puro", goles: 0 }
+        ],
+        equipoRojo: [
+            { nombre: "Juan López", goles: 1 },
+            { nombre: "Clarico", goles: 0 },
+            { nombre: "Floris", goles: 3 }
+        ]
+    },
+    {
+        jornada: 2,
+        fecha: "08/09/2025",
+        arbitro: "Villegas",
+        equipoNegro: [
+            { nombre: "Santi", goles: 1 },
+            { nombre: "Pedro", goles: 0 },
+            { nombre : "Floris", goles: 7 }
+        ],
+        equipoRojo: [
+            { nombre: "Juan López", goles: 2 },
+            { nombre: "Catrasto", goles: 1 }
+        ]
+    }
+];
